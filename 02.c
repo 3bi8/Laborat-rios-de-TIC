d76046141e87babb9fa8960f9f2bcaf003968db8
3bi8 <90455236+3bi8@users.noreply.github.com>
2021-10-02 18:27:32 +0100

    Update 02.c

--- 02.c
+++ 02.c
@@ -3,7 +3,7 @@
 	
 		float sal_min, sal_fun;
 		int  quant=0;
-		printf("Digite o salario minimo e o seu:\n);
+		printf("Digite o salario minimo e o seu:\n");
 		scanf("%f%f", &sal_min, &sal_fun);
 		quant = sal_fun/sal_min;
 		printf("No seu salario temos %i salarios minimos",quant);
